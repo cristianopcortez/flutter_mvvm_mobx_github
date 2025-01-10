09e44c22e61355835dfd4a9d709742393214793e
Cristiano Cortez <cristianocortez@gmail.com>
2025-01-10 13:14:31 -0300

    Changing intro video... Fixes on startup to deal better with runZonedGuarded problems....

--- lib/main.dart
+++ lib/main.dart
@@ -14,100 +14,97 @@ import 'features/view/ProductHomePage.dart';
 
 void main() async {
 
-  final logger = Logger();
+  BindingBase.debugZoneErrorsAreFatal = false; // Makes zone errors fatal during debugging
 
-  logger.i("Info message"); // Information
-  logger.e("Error message"); // Error
-  logger.d("Debug message"); // Debug
-
-  stderr.writeln("This should appear in logcat.");
-
-  // FlutterError.onError = (FlutterErrorDetails details) {
-  //   FlutterError.presentError(details);
-  //   // Log to your backend or analytics
-  //   // if (kDebugMode) {
-  //     print(details.exceptionAsString());
-  //   // }
-  // };
-
-  BindingBase.debugZoneErrorsAreFatal = true; // Makes zone errors fatal during debugging
+  // Ensure Flutter bindings are initialized before anything else
   WidgetsFlutterBinding.ensureInitialized();
 
-    String apiKey = Platform.isAndroid
-        ? const String.fromEnvironment('FIREBASE_API_KEY') ?? ''
-        : '';
-    String appID = Platform.isAndroid
-        ? const String.fromEnvironment('FIREBASE_APP_ID') ?? ''
-        : '';
-    String firebaseMessagingSenderID = Platform.isAndroid
-        ? const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID') ?? ''
-        : '';
-    String firebaseProjectID = Platform.isAndroid
-        ? const String.fromEnvironment('FIREBASE_PROJECT_ID') ?? ''
-        : '';
-    String firebaseProjectStorageBucket = Platform.isAndroid
-        ? const String.fromEnvironment('FIREBASE_STORAGE_BUCKET') ?? ''
-        : '';
-    // if (kDebugMode) {
-      debugPrint("Firebase.initializeApp");
-    // }
-  try {
-    await Firebase.initializeApp(
-      options: firebaseOptions(
-        apiKey,
-        firebaseMessagingSenderID,
-        appID,
-        firebaseProjectID,
-        firebaseProjectStorageBucket,
-      ),
-    ).whenComplete(() {
-      // if (kDebugMode) {
-      logger.d("Initialization completed");
-      debugPrint("Initialization completed");
-      // }
-    });
-  } catch (e, stackTrace) {
-    logger.e(
-        "Error Firebase.initializeApp",
-        time: DateTime.now(),
-        error: e,
-        stackTrace: stackTrace
-    );
-  }
-    //     .catchError((e) {
-    //   logger.e("Error Firebase.initializeApp"); // Error
-    // })
-    //     .catchError((e) {
-    //   // if (kDebugMode) {
-    //     debugPrint("Exception Type: ${e.runtimeType}");
-    //   // }
-    //   if (e is FirebaseException) {
-    //     // if (kDebugMode) {
-    //       debugPrint("Firebase Error Code: ${e.code}");
-    //       debugPrint("Firebase Error Message: ${e.message}");
-    //     // }
-    //   } else {
-    //     // if (kDebugMode) {
-    //       debugPrint("Unknown Error:");
-    //       debugPrint(e.toString());
-    //     // }
-    //   }
-    // })
+  final logger = Logger();
 
-  // Enable Crashlytics
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  // logger.i("Info message"); // Information
+  // logger.e("Error message"); // Error
+  // logger.d("Debug message"); // Debug
+
+  // stderr.writeln("This should appear in logcat.");
+
+  // Temporary local error logger
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    // Log to your backend or analytics
+    // if (kDebugMode) {
+    //   print(details.exceptionAsString());
+    logger.e(details.exceptionAsString());
+    // }
+  };
+
+  String apiKey = Platform.isAndroid
+      ? const String.fromEnvironment('FIREBASE_API_KEY') ?? ''
+      : '';
+  String appID = Platform.isAndroid
+      ? const String.fromEnvironment('FIREBASE_APP_ID') ?? ''
+      : '';
+  String firebaseMessagingSenderID = Platform.isAndroid
+      ? const String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID') ?? ''
+      : '';
+  String firebaseProjectID = Platform.isAndroid
+      ? const String.fromEnvironment('FIREBASE_PROJECT_ID') ?? ''
+      : '';
+  String firebaseProjectStorageBucket = Platform.isAndroid
+      ? const String.fromEnvironment('FIREBASE_STORAGE_BUCKET') ?? ''
+      : '';
 
   runZonedGuarded(() async {
-    runApp(
-      Provider<VideoPlayerControllerStore>(
-        create: (_) => VideoPlayerControllerStore(),
-        child: const MyApp(),
-      ),
-    );
+    // if (kDebugMode) {
+    debugPrint("Firebase.initializeApp");
+    // }
+    try {
+      await Firebase.initializeApp(
+        options: firebaseOptions(
+          apiKey,
+          firebaseMessagingSenderID,
+          appID,
+          firebaseProjectID,
+          firebaseProjectStorageBucket,
+        ),
+      );
+
+      // Firebase initialization succeeded
+      logger.d("Firebase initialization completed");
+      debugPrint("Firebase initialization completed");
+
+      // Ensure Crashlytics Logs During Debugging
+      FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+      // Enable Crashlytics
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+      runApp(
+        Provider<VideoPlayerControllerStore>(
+          create: (_) => VideoPlayerControllerStore(),
+          child: const MyApp(),
+        ),
+      );
+
+    } catch (e, stackTrace) {
+      // Handle Firebase initialization failure
+      logger.e(
+        "Error during Firebase.initializeApp",
+        time: DateTime.now(),
+        error: e,
+        stackTrace: stackTrace,
+      );
+    }
+
   }, (error, stackTrace) {
     // Log to your backend or analytics
-    print('Caught by runZonedGuarded: $error');
-    print('Stack trace: $stackTrace');
+    // print('Caught by runZonedGuarded: $error');
+    // print('Stack trace: $stackTrace');
+    logger.e(
+      "Caught by runZonedGuarded: ",
+      time: DateTime.now(),
+      error: error,
+      stackTrace: stackTrace,
+    );
   });
 }
 
